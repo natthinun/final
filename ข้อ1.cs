78546653461243d9a5c0b135b919eca74181524a
natthix <natthinun.blue@mail.kmutt.ac.th>
2021-06-13 16:52:31 +0700

    แก้

---
+++ ข้อ1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int higth = int.Parse(Console.ReadLine());
+            char[,] numarray = new char[width, higth];
+            for (int i = 0; i < higth; i++) ;
+            {
+                for (int j = 0; j < width; j++) ;
+                {
+                    numarray[j, i] = char.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < width; i++) ;
+            {
+                for (int j = 0; j < higth; j++) ;
+                {
+                    Console.Write(numarray[j, i]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
