315294e07f839e4fc3860e6ab7141a2852f150d7
natthix <natthinun.blue@mail.kmutt.ac.th>
2021-06-13 17:09:52 +0700

    ข้อ6

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number from 0-3 th choose the following menu:");
+            Console.WriteLine("[0]Show all recent files");
+            Console.WriteLine("[1]Open new file");
+            Console.WriteLine("[2]Open recent file");
+            Console.WriteLine ("[3]Exit");
+            
+            Console.Write("Your select : ");
+            string menu = Console.ReadLine();
+
+            StreamWriter file = null;
+            file = null;
+            string filename = @"textfile.tet";
+            string line = null;
+
+            Console.Write("enter your message :: ");
+            line = Console.ReadLine();
+            file.WriteLine(line);
+
+            try
+            {
+                if (menu == "0")
+                {
+                    file = new StreamWriter(filename);
+
+                }
+                else if (menu == "1")
+                {
+                    file = File.AppendText(filename);
+                }
+
+                else if (menu == "2")
+                {
+                    file = File.CreateText(filename);
+                }
+                else if (menu == "3")
+                {
+                    file.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("No recent file.");
+            }
+            finally
+            {
+                if(file != null)
+                {
+                    file.Close();
+                }
+            }
+
+            file.Close();
+
+      
+
+        }
+    }
+}
